9935ead2880a5721d5d55db0b077e5fc6b92100c
Rain06 <ramazanzhilkibay@gmail.com>
2025-12-11 12:10:05 +0500

    fixed cors

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_BASE = import.meta.env.VITE_API_URL || '/api'
+const API_BASE = import.meta.env.VITE_API_URL || 'https://cp-helper-32yt.onrender.com/api'
+
 
 const axiosAuth = axios.create({
   baseURL: API_BASE
